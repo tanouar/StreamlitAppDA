034059e36ae6b15b91fa9ad73568f10270a3ef65
ClaireM63 <claire.moro@gmail.com>
2024-03-12 18:42:36 +0100

    encore test image

--- tabs/intro.py
+++ tabs/intro.py
@@ -13,7 +13,7 @@ def run():
     with st.expander("Comment les gaz à effet de serre contribuent-ils au réchauffement de la planète ?"):
         st.write("Les gaz à effet de serre sont des composés présents dans l'atmosphère qui absorbent et émettent le rayonnement infrarouge, contribuant ainsi au réchauffement de la planète. Les principaux gaz à effet de serre d'origine humaine sont le dioxyde de carbone (CO2), le méthane (CH4) et le protoxyde d'azote (N2O).")
     
-    st.image("tabs\chgtClim.jpg")
+    st.image("Data/chgtClim.jpg")
     # st.image("https://static.vecteezy.com/ti/vecteur-libre/p1/16921140-concept-d-illustration-de-changement-climatique-sur-fond-blanc-vectoriel.jpg", width=600)
     st.write(PROJECT_DESCRIPTION)
     
@@ -21,14 +21,13 @@ def run():
     st.write('➽ Collecter et prétraiter des données sur les températures terrestres et les gaz à effet de serre ')
     st.write('➽ Utiliser des techniques de visualisation pour représenter graphiquement les variations de température au fil du temps et leur relation avec les concentrations de gaz à effet de serre')
     st.write('➽ Effectuer des analyses statistiques pour quantifier les relations entre les températures et les gaz à effet de serre')
-    st.write('➽ Développer des modèles de prédiction en utilisant des techniques de modélisation et de machine learning')
+    st.write('➽ Développer des modèle  s de prédiction en utilisant des techniques de modélisation et de machine learning')
 
     
     st.write("")
     st.write("")
     st.write("")
     st.write("")
-    
 
 
 
