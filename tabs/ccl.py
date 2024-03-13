c3591509372f9e03f97da60a4a09ff97fc9a2d7d
ClaireM63 <claire.moro@gmail.com>
2024-03-13 10:27:44 +0100

    few modif CCL

--- tabs/ccl.py
+++ tabs/ccl.py
@@ -6,9 +6,7 @@ sidebar_name = "Conclusions & Perspectives"
 
 
 def run():
-    st.image("Data/rechauffement-climatique.jpg", width=400)
-
-
+   #  st.image("Data/rechauffement-climatique.jpg", width=400)
     st.header(sidebar_name)
     st.markdown("---")
 
@@ -17,7 +15,7 @@ def run():
        st.write("Le sujet de l’évolution des températures en corollaire de la production des gaz à effet de serre est très largement étudié depuis des décennies, mettant en évidence un impact des activités humaines sur ce qu’il est commun d’appeler le réchauffement climatique.")
     
     with st.expander("**Rappel des objectifs**"):
-       st.write("➽ Collecter et prétraiter des données sur les températures terrestres et les gaz à effet de serre.\n\n➽ Utiliser des techniques de visualisation pour représenter graphiquement les variations de température au fil du temps et leur relation avec les concentrations de gaz à effet de serre.\n\n➽ Effectuer des analyses statistiques pour quantifier les relations entre les températures et les gaz à effet de serre.\n\n➽ Développer des modèles de prédiction en utilisant des techniques de modélisation et de *Machine Learning*.")
+       st.write("➽ Collecter et prétraiter des données sur les températures terrestres et les gaz à effet de serre.\n\n➽ Utiliser des techniques de visualisation pour représenter graphiquement les variations de température au fil du temps et leur relation avec les emissions de gaz à effet de serre.\n\n➽ Effectuer des analyses statistiques pour quantifier les relations entre les températures et les gaz à effet de serre.\n\n➽ Développer des modèles de prédiction en utilisant des techniques de modélisation et de *Machine Learning*.")
     
     with st.expander("**Sources de données**"):
        st.write("Nous disposons de sources de données publiques fiables, combinant d’un côté les productions annuelles par pays des différents gaz à effet de serre et l’évolution relative de la température terrestre qui en découle, et d’autre part les données d’évolution relative de la température terrestre par pays.\n\nCes données sont par ailleurs mesurées ou extrapolées depuis l’ère préindustrielle jusqu’à nos jours.")
@@ -27,5 +25,12 @@ def run():
     
     with st.expander("**Perspectives**"):
        st.write("➽ Utiliser les mesures atmosphériques disponibles des différents gaz à effet de serre pour recherche de corrélation et prédiction d’évolution de températures terrestres.\n\n➽ Utiliser des regroupements de pays sur d’autres critères que géographiques (par le niveau de développement, profils de production des gaz à effet de serre, …).\n\n➽ Obtenir une prédiction meilleure pour les gaz à effet de serre hors CO2, uniquement pour les périodes récentes et à venir, ces données étant largement manquantes par pays à part pour les dernières décennies.\n\n➽ Prendre en compte de multiples scénarios de stabilisation de la température terrestre globale (tels que ratifiés dans l’accord de Paris), que ce soit sur les années ou décennies futures comme récentes (cf. inflexion détectée depuis quelques décennies de production de certains gaz à effet de serre par certains pays).")
+
+
+
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
 
 
