202b09f37072ae48fb628ed8dfacf09f8afee04f
Mar24DA <166515662+Mar24DA@users.noreply.github.com>
2024-04-09 14:39:44 +0200

    Init
    Premier fichier py pour le Streamlit

---
+++ streamlit_velo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Traffic cycliste à Paris")
+st.write("Projet")
+if st.checkbox("Afficher"):
+  st.write("Analyser les comptages sur les années 2022 et 2023")
+  st.write("Prédire pour l'année 2024")
